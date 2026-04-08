c61ce534d9fb27b0625cf1735a85abefc93f8d66
Soham Rathod <sohamrathod4567@gmail.com>
2026-04-08 11:41:46 +0530

    refactor: standardize quotes and formatting in Data.js

--- backend/model/Data.js
+++ backend/model/Data.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const dataSchema = new mongoose.Schema(
   {
     voltage: { type: Number, required: true },
     current: { type: Number, required: true },
     temperature: { type: Number, required: true },
-    timestamp: { type: Date, default: Date.now }
+    timestamp: { type: Date, default: Date.now },
   },
   {
     versionKey: false,
@@ -13,10 +13,10 @@ const dataSchema = new mongoose.Schema(
       transform: (_, ret) => {
         delete ret._id;
         return ret;
-      }
-    }
-  }
+      },
+    },
+  },
 );
 
-const Data = mongoose.model('Data', dataSchema);
+const Data = mongoose.model("Data", dataSchema);
 export default Data;
