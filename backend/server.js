b679c7075794f922b5ca38bfbf4a4efe435d88e8
Soham Rathod <sohamrathod4567@gmail.com>
2026-04-08 12:12:23 +0530

    JS Edit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes.js";
 import dataRoutes from "./routes/dataRoutes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 app.use("/api", dataRoutes);
 
 app.use((err, req, res, next) => {
