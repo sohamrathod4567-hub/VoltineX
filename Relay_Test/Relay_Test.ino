5a92ae015ec73b46a3436eb4878e8046a5f6b86e
Soham Rathod <sohamrathod4567@gmail.com>
2026-04-24 18:38:05 +0530

    Relay Test

---
+++ Relay_Test/Relay_Test.ino
@@ -0,0 +1,32 @@
+//OK
+const int M_Relay=6;
+const int AXP_Relay=5;
+const int AXS_Relay=4;
+void setup()
+{
+  pinMode(M_Relay,OUTPUT);
+  pinMode(AXP_Relay,OUTPUT);
+  pinMode(AXS_Relay,OUTPUT);
+  digitalWrite(M_Relay,HIGH);
+  delay(500);
+  digitalWrite(AXP_Relay,HIGH);
+  delay(500);
+  digitalWrite(AXS_Relay,HIGH);
+  delay(500);
+}
+void loop()
+{
+   digitalWrite(M_Relay,LOW);
+   delay(1000);
+   digitalWrite(AXP_Relay,LOW);
+   delay(1000);
+   digitalWrite(AXS_Relay,LOW);
+   delay(5000);
+   digitalWrite(M_Relay,HIGH);
+   delay(1000);
+   digitalWrite(AXP_Relay,HIGH);
+   delay(1000);
+   digitalWrite(AXS_Relay,HIGH);
+   delay(1000);
+}
+
